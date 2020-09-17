7e9052ac975e46553d1e4b0669b895eb4c4e3205
Jeff-Adler <Jeffm.adler@gmail.com>
2020-09-17 18:30:28 -0400

    9-17_evening

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,4 +1,4 @@
 class Connection < ApplicationRecord
-  belongs_to :mentee, class_name: :User #, foreign_key: :user_id, optional: true
-  belongs_to :mentor, class_name: :User #, foreign_key: :user_id, optional: true
+  belongs_to :mentee, class_name: :User 
+  belongs_to :mentor, class_name: :User 
 end
