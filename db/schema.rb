d0b33373df85dab2a10a749144582dbcb487c000
Jeff-Adler <Jeffm.adler@gmail.com>
2020-09-18 13:44:09 -0400

    fixed signup routing

--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2020_09_17_005127) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.string "birthdate"
     t.string "gender"
     t.string "avatar"
