f3207af30ebcf4503a11ea9778ab7b9f9788b72d
Jeff-Adler <Jeffm.adler@gmail.com>
2020-09-19 16:24:54 -0400

    implement birthdate update route

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -17,11 +17,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def update
-    @user.update_attribute(:username, user_edit_params[:username]) unless user_edit_params[:username] == null
-    @user.update_attribute(:birthdate, user_edit_params[:birthdate]) unless user_edit_params[:birthdate] == null
-    @user.update_attribute(:gender, user_edit_params[:gender]) unless user_edit_params[:gender] == null
-    @user.update_attribute(:avatar, user_edit_params[:avatar]) unless user_edit_params[:avatar] == null
-    @user.update_attribute(:karma, user_edit_params[:karma]) unless user_edit_params[:karma] == null
+    @user.update(user_edit_params)
     render json: {user: UserSerializer.new(@user)}
   end
  
@@ -32,7 +28,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def user_edit_params
-    params.require(:user).permit(:username,:birthdate,:gender,:avatar,:karma)
+    params.require(:user).permit(:username,:birthdate,:gender,:avatar,:karma).select { |k, v| !v.nil? }
   end
 
   def user_params
