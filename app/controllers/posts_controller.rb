b25b42ce00f75fc680b3488b1b5ff4d30bcfe6c5
Jeff-Adler <Jeffm.adler@gmail.com>
2020-09-18 21:32:02 -0400

    9_18_2020_evening

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,6 @@
 class PostsController < ApplicationController
 
     def index 
-        #Return:
-        # Post.mentor
-        # Post.mentee
-        # @post.connection.mentor/mentee works!
         render json: Post.all
     end
 end
