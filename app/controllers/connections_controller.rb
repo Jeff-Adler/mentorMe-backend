0dc9f078d5595cbc2534180662a0fdec9c0b960d
Jeff-Adler <Jeffm.adler@gmail.com>
2020-09-19 20:38:09 -0400

    9-19-2020

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -1,2 +1,12 @@
 class ConnectionsController < ApplicationController
+    def create 
+        connection = Connection.create(connection_params)
+        render json: connection
+    end
+  
+    private
+  
+    def connection_params
+        params.require(:connection).permit(:mentee_id,:mentor_id)
+    end
 end
