1c15246322f041fbc80851a684810561001b3f07
Jeff-Adler <Jeffm.adler@gmail.com>
2020-09-21 13:39:02 -0400

    wrote posts#show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,17 @@
 class PostsController < ApplicationController
+    before_action :find_post, only: [:show]
 
     def index 
         render json: Post.all
+    end
+
+    def show 
+        render json: @post
+    end
+
+    private
+
+    def find_post
+        @post = Post.find(params[:id])
     end
 end
