bd990fb655f564536d52830baa6e64c7a65ab11b
Jeff-Adler <Jeffm.adler@gmail.com>
2020-10-01 18:10:57 -0400

    seed changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,6 @@ MENTOR_TYPES = [
 ]
 
 DESCRIPTION_BLURBS = [
-    "HS student looking to improve my self-esteem.",
-    "College student looking to work on my romanitc relationships.",
     "College student looking to find some hobbies.",
     "College student looking to be less shy.",
     "Early-career professional looking to get direction.",
@@ -23,10 +21,11 @@ DESCRIPTION_BLURBS = [
     "Early-career professional looking to become a yoga teacher.",
     "Career changer looking to break into software engineering!",
     "Career changer looking to break into freelance!",
-    "Career changer looking to learn more about entrepeneurship!"
+    "Career changer looking to learn more about entrepeneurship!",
+    "Tech enthusiast looking to work on my romantic relationships."
 ]
 
-for i in 0..30 do 
+for i in 0..25 do 
     gender = (i % 2 == 0) ? "male" : "female"
     name = (gender == "female") ? Faker::Name.unique.female_first_name : Faker::Name.unique.male_first_name
     begin
@@ -65,6 +64,30 @@ for i in 0..8 do
         last_name: Faker::Name.last_name,
         password: Faker::Alphanumeric.alpha(number: 10),
         birthdate: Faker::Date.birthday(min_age: 45, max_age: 65),
+        gender: gender,
+        avatar: userImage,
+        professional: (rand(0..1) == 0 ? true : false),
+        interpersonal: (rand(0..1) == 0 ? true : false),
+        self_improvement: (rand(0..1) == 0 ? true : false),
+        description: DESCRIPTION_BLURBS[rand(0..(DESCRIPTION_BLURBS.length - 1))]
+    )
+end
+
+for i in 0..8 do 
+    gender = (i % 2 == 0) ? "male" : "female"
+    name = (gender == "female") ? Faker::Name.unique.female_first_name : Faker::Name.unique.male_first_name
+    begin
+        response = HTTParty.get("https://randomuser.me/api/?gender=#{gender}&noinfo&?seed=#{name}")
+        userImage = JSON.parse(response.body)["results"][0]["picture"]["large"]
+    rescue JSON::ParserError
+        userImage = nil
+    end
+    user = User.create!(
+        username: Faker::Internet.unique.username(specifier: 7..12),
+        first_name: name,
+        last_name: Faker::Name.last_name,
+        password: Faker::Alphanumeric.alpha(number: 10),
+        birthdate: Faker::Date.birthday(min_age: 20, max_age: 29),
         gender: gender,
         avatar: userImage,
         professional: (rand(0..1) == 0 ? true : false),
